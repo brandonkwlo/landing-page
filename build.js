02e72fa472697e869c1d1c6c411d68a308bddb45
brandonkwlo <brandonkwlo@gmail.com>
2024-08-19 16:27:25 -0700

    Updated build.js 2

--- build.js
+++ build.js
@@ -1,8 +1,4 @@
 const fs = require("fs");
-
-const html = fs.readFileSync("index.html", "utf8");
-const newHtml = html.replace("%POSTHOG_API_KEY%", process.env.POSTHOG_API_KEY);
-fs.writeFileSync("index.html", newHtml);
 const path = require("path");
 
 // Create public directory if it doesn't exist
@@ -11,14 +7,29 @@ if (!fs.existsSync(publicDir)) {
   fs.mkdirSync(publicDir);
 }
 
+// Function to copy file or directory
+function copyRecursive(src, dest) {
+  if (fs.lstatSync(src).isDirectory()) {
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest);
+    }
+    fs.readdirSync(src).forEach((childItemName) => {
+      copyRecursive(
+        path.join(src, childItemName),
+        path.join(dest, childItemName)
+      );
+    });
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
+
 // Copy your HTML, CSS, and other assets to the public directory
+copyRecursive("css", path.join(publicDir, "css"));
+copyRecursive("assets", path.join(publicDir, "assets"));
 fs.copyFileSync("index.html", path.join(publicDir, "index.html"));
-fs.copyFileSync(
-  "/assests/img/bg-signup.jpg",
-  path.join(publicDir, "/assests/img/bg-signup.jpg")
-);
-fs.copyFileSync(
-  "/assests/favicon.ico",
-  path.join(publicDir, "/assests/favicon.ico")
-);
-fs.copyFileSync("/css/styles.css", path.join(publicDir, "/css/styles.css"));
+
+// Replace PostHog API key in index.html
+let html = fs.readFileSync(path.join(publicDir, "index.html"), "utf8");
+const newHtml = html.replace("%POSTHOG_API_KEY%", process.env.POSTHOG_API_KEY);
+fs.writeFileSync(path.join(publicDir, "index.html"), newHtml);
